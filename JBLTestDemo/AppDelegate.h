5dd4248fa3a1086aaf5fbe93377794359181a716
Aaron Hou <Aaron@joeys-imac.local>
2017-12-20 18:01:17 +0800

    test

---
+++ JBLTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JBLTestDemo
+//
+//  Created by Aaron Hou on 20/12/2017.
+//  Copyright © 2017 Aaron Hou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
